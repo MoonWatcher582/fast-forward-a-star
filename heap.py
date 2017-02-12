3cc5472772980141c3e3ab4b807a11a55fc5c8c4
MoonWatcher582 <erbronne@gmail.com>
2017-02-11 20:47:19 -0500

    Fix index out of bounds problem

--- heap.py
+++ heap.py
@@ -31,14 +31,28 @@ class MinHeap(object):
 	def sift_down(self, index):
 		r_index = self.right_child(index)
 		l_index = self.left_child(index)
-		#if the root is less than both of its children, it is in the correct position; break
-		if self.heap_data[index] < self.heap_data[r_index] and self.heap_data[index] < self.heap_data[l_index]:
-			return
-		else:
-			if self.heap_data[r_index] < self.heap_data[l_index]:
-				smaller = r_index
+		# if the root is less than both of its children, it is in the correct position; break
+		if r_index < self.size:
+			if self.heap_data[index] < self.heap_data[r_index]: 
+				if l_index < self.size:
+					if self.heap_data[index] < self.heap_data[l_index]:
+						return
+				else:
+					return
 			else:
+				return
+		else:
+			if r_index < self.size and l_index < self.size:
+				if self.heap_data[r_index] < self.heap_data[l_index]:
+					smaller = r_index
+				else:
+					smaller = l_index
+			elif r_index < self.size and l_index >= self.size:
+				smaller = r_index
+			elif r_index >= self.size and l_index < self.size:
 				smaller = l_index
+			else:
+				return
 			self.swap(index, smaller)
 		self.sift_down(smaller)
 
