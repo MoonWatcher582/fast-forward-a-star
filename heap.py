5bbde5cf4ed8e1140b6a218ca287ec48a989ed14
MoonWatcher582 <erbronne@gmail.com>
2017-02-11 19:17:23 -0500

    Add heap and maze generator with 50 mazes

---
+++ heap.py
@@ -0,0 +1,63 @@
+import math
+
+class MinHeap(object):
+	def __init__(self):
+		self.heap_data = []
+		self.size = 0
+
+	def insert(self, data):
+		self.heap_data.append(data)
+		self.size += 1
+		self.sift_up(len(self.heap_data) - 1)
+
+	def extract(self):
+		temp = self.heap_data[0]
+		self.heap_data[0] = self.heap_data[len(self.heap_data) - 1]
+		del(self.heap_data[-1])
+		self.size -= 1
+		self.sift_down(0)
+		return temp
+
+	def sift_up(self, index):
+		while index != 0:
+			p = self.parent(index)
+			p = int(p)
+			if self.heap_data[index] < self.heap_data[p]:
+				self.swap(index, p)
+				index = p
+			else:
+				break
+
+	def sift_down(self, index):
+		r_index = self.right_child(index)
+		l_index = self.left_child(index)
+		#if the root is less than both of its children, it is in the correct position; break
+		if self.heap_data[index] < self.heap_data[r_index] and self.heap_data[index] < self.heap_data[l_index]:
+			return
+		else:
+			if self.heap_data[r_index] < self.heap_data[l_index]:
+				smaller = r_index
+			else:
+				smaller = l_index
+			self.swap(index, smaller)
+		self.sift_down(smaller)
+
+	def heapsort(self):
+		for i in self.size:
+			self.sift_up(i)
+		for i in self.size:
+			self.sift_down(self.size - i -1)	
+
+	def swap(self, ind1, ind2):
+		temp = self.heap_data[ind1]
+		self.heap_data[ind1] = self.heap_data[ind2]
+		self.heap_data[ind2] = temp
+
+	def parent(self, index):
+		return math.floor((index - 1) / 2)
+
+	def left_child(self, index):
+		return ((index * 2) + 1)
+
+	def right_child(self, index):
+		return ((index * 2) + 2)
---
+++ maze_gen.py
@@ -0,0 +1,100 @@
+import random
+from collections import deque
+
+class Node(object):
+	def __init__(self, x, y):
+		self.visited = False
+		self.blocked = False
+		self.x = x
+		self.y = y
+
+	def setBlocked(self):
+		self.blocked = True
+
+class Grid(object):
+	def __init__(self):
+		self.grid = []
+		for i in range(101):
+			self.grid.append([])
+			for j in range(101):
+				self.grid[i].append(Node(j, i))
+	
+	def generate_maze(self):
+		if not self.grid:
+			raise Exception("No grid found")
+
+		self.start_x = random.randint(0, 100)
+		self.start_y = random.randint(0, 100)
+
+		self.grid = dfs(self, self.grid[self.start_y][self.start_x])
+		return self 
+	
+	def find_unexpanded_node(self):
+		if not self.grid:
+			raise Exception("No grid found")
+
+		for x in range(101):
+			for y in range(101):
+				if self.grid[y][x].visited == False:
+					return self.grid[y][x]
+
+		return None 
+
+	def generate_neighbors(self, x, y):
+		if not self.grid:
+			raise Exception("No grid found")
+
+		for x_ in range(max(0, x-1), min(100, x+1)):
+			for y_ in range(max(0, y-1), min(100, y+1)):
+				if (x, y) == (x_, y_):
+					continue
+				yield self.grid[y_][x_]
+
+def dfs(grid, start):
+	visited_set = set()
+	stack = deque()
+	stack.append(start)
+
+	while len(visited_set) != 101:
+		while stack:
+			vertex = stack.pop()
+			if not vertex.visited:
+				if (random.random() * 100) <= 30.0:
+					vertex.blocked = True 
+				vertex.visited = True
+				visited_set.add(vertex)
+				for neighbor in grid.generate_neighbors(vertex.x, vertex.y):
+					stack.append(neighbor)
+		n = grid.find_unexpanded_node()
+		if not n:
+			break
+		stack.append(n)
+
+	grid.grid[grid.start_y][grid.start_x].blocked = False
+	return grid.grid
+
+def main():
+	for i in range(50):
+		maze_index = i
+		if maze_index < 10:
+			maze_index = '0' + str(maze_index)
+		else:
+			maze_index = str(maze_index)
+		print "Generating maze " + maze_index
+		maze = Grid().generate_maze()
+		with open('mazes/maze' + maze_index, 'w') as f:
+			# Start state coordinates
+			f.write(str(maze.start_x) + "," + str(maze.start_y) + "\n")
+			# End state coordinates
+			f.write(str(random.randint(0, 100)) + "," + str(random.randint(0, 100)) + "\n")
+			for x in range(101):
+				for y in range(101):
+					coordValue = '0'
+					if maze.grid[y][x].blocked:
+						coordValue = '1'
+					f.write(coordValue)
+				if x != 100:
+					f.write('\n')
+
+if __name__ == "__main__":
+	main()
