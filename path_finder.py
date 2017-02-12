4f55d38f1bb9fa09e103b5a19782032968dd8313
MoonWatcher582 <erbronne@gmail.com>
2017-02-11 20:59:17 -0500

    Fix coord print

--- path_finder.py
+++ path_finder.py
@@ -7,9 +7,10 @@ class Coord:
     def __init__(self, x, y):
         self.x=x
         self.y=y
+		  self.__repr__ = self.__str__
 
     def __str__(self):
-        return str(self.x) + ", " + str(self.y)
+        return "(" + str(self.x) + ", " + str(self.y) + ")"
 
     def __lt__(self, other):
         return self.distance < other.distance
