cd352292f14c98971249081a7039fdc16bc0610d
katiepru <katiepru@nbcs.rutgers.edu>
2017-02-11 19:30:22 -0500

    Adding path finder class

---
+++ path_finder.py
@@ -0,0 +1,17 @@
+class Coord:
+    def __init__(self, x, y):
+        self.x=x
+        self.y=y
+
+class PathFinder:
+    def __init__(self, maze_file):
+        # TODO: read maze into 2D  array.
+        pass
+
+    def FindPath(self, start, goal):
+        # TODO: A* here.
+        pass
+
+    def CalculateManhattanDistance(self, start, end):
+        #TODO: Manhattan distance calucation.
+        pass
